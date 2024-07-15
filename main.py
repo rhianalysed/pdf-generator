6f35890c36b1d7eaf9a0599f84fe5385046a9a64
Rhiann <rhiann264@hotmail.co.uk>
2024-07-15 18:04:29 +0100

    Created a lined page

--- main.py
+++ main.py
@@ -14,7 +14,9 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=12)
     pdf.set_text_color(0, 0, 102)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(x1=10, y1=22, x2=200, y2=22)
+    # Creating the lined page
+    for y in range(20, 298, 10):
+        pdf.line(x1=10, y1=y, x2=200, y2=y)
 
     # Creating the footer
     pdf.ln(265)
@@ -28,5 +30,9 @@ for index, row in df.iterrows():
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(100, 100, 100)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+        # Creating the lined page
+        for y in range(20, 298, 10):
+            pdf.line(x1=10, y1=y, x2=200, y2=y)
+
 
 pdf.output("output.pdf")
